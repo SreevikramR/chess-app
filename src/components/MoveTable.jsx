fa68fd768319c49d1923dbe9345bd73b75602378
Sreevikram R <sreevikram.r@gmail.com>
2023-03-20 11:51:10 -0500

    Reusability increased

--- src/components/MoveTable.jsx
+++ src/components/MoveTable.jsx
@@ -1,16 +1,33 @@
+import React, { useEffect } from "react";
+import './styles/MoveTable.css'
 
-const MoveTable = ({ moveSequence, moves, openingLine }) => {
+let moveIds  = []
+let correctMoves = []
+let wrongMove;
+let previousMoveSequence;
+
+const MoveTable = ({ moveSequence, moves, openingLine, moveResult }) => {
     let sequenceLength = moveSequence.length
     let numRows = Math.ceil(sequenceLength / 2);
+    if(previousMoveSequence !== moveSequence) {
+        previousMoveSequence = moveSequence
+        correctMoves = []
+        wrongMove = ''
+        moveIds = []
+    }
 
     let rows = [];
 
     for (let i = 0; i < numRows; i++) {
+
         let rowNumber = i + 1;
         let whiteMove = moves[i * 2];
         let blackMove = moves[i * 2 + 1];
     
         let whiteCellId = whiteMove !== undefined ? "ti" + (i * 2) : "";
+        if(whiteCellId !== "") {
+            moveIds.push(whiteCellId)
+        }
         let blackCellId = blackMove !== undefined ? "ti" + (i * 2 + 1) : "";
     
         let row = (
@@ -22,7 +39,34 @@ const MoveTable = ({ moveSequence, moves, openingLine }) => {
         );
     
         rows.push(row);
-      }
+    }
+
+    let lastMove = moveIds[moveIds.length - 1]
+    console.log(lastMove)
+    if(moveResult === 'correct' && lastMove !== undefined && lastMove !== correctMoves[correctMoves.length - 1]) {
+        correctMoves.push(lastMove)
+        wrongMove = ''
+    } else if (moveResult === 'wrong') {
+        wrongMove = lastMove
+        console.log('move is wrong')
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (correctMoves.length !== 0 || wrongMove) {
+                for (let j = 0; j < correctMoves.length; j++) {
+                    console.log(correctMoves[j])
+                    document.getElementById(correctMoves[j]).className = ''
+                    document.getElementById(correctMoves[j]).classList.add('correct')
+                }
+                if (wrongMove) {
+                    document.getElementById(wrongMove).className = ''
+                    document.getElementById(wrongMove).classList.add('wrong')
+                }
+            }
+        }, 50)
+    
+    }, [moves])
 
     return (
         <table>
--- src/pages/TrialPage.jsx
+++ src/pages/TrialPage.jsx
@@ -1,147 +1,37 @@
 import './styles/TrialPage.css'
 import { getLineIndex, getAlternateLine, getMoveSequence } from "../components/MoveRetreival"
-import { useState, useEffect } from 'react'
-import { Chess } from 'chess.js'
-import { Chessboard } from 'react-chessboard'
-import MoveSelector from '../components/MoveSelector'
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import MoveTable from '../components/MoveTable'
+import TrainBoardWhite from '../components/TrainBoardWhite'
 
 let openeingName = 'Ruy Lopez';
-let openingLine = 'Cozio Defense';
-let movePlayed;
-let moveMessage;
 let moveSequence = [];
-let moveHistory = []
 
 function TrialPage() {
-
-    var viewPortWidth = window.innerWidth;
-    var viewPortHeight = window.innerHeight;
+    const [moveHistory, setMoveHistory] = useState([])
+    const [openingLine, setOpeningLine] = useState('Cozio Defense')
+    const [moveResult, setMoveResult] = useState('correct')
     let openingLineIndex = getLineIndex(openeingName, openingLine);
     moveSequence = getMoveSequence(openeingName, openingLine);
 
-    const [game, setGame] = useState(new Chess());
-    const [position, setPosition] = useState();
-    const [boardWidth, setBoardWidth] = useState(500);
-    let nextMove;
-
-    window.setTimeout(function(){
-        viewPortWidth = window.innerWidth;
-        viewPortHeight = window.innerHeight;
-        
-        if(viewPortWidth/2 > 500){
-            setBoardWidth(500);
-        } else {
-            setBoardWidth(viewPortWidth/2);
-        }
-    }, 100)
-
-    viewPortWidth = window.innerWidth;
-    viewPortHeight = window.innerHeight;
-
-    window.onresize = function() {
-        viewPortWidth = window.innerWidth;
-        viewPortHeight = window.innerHeight;
-        
-        if(viewPortWidth/2 > 500){
-            setBoardWidth(500);
-        } else {
-            setBoardWidth(viewPortWidth/2.2);
-        }
-    };
-
     useEffect(() => {
-        viewPortWidth = window.innerWidth;
-        viewPortHeight = window.innerHeight;
-    
-        if(viewPortWidth/2 > 500){
-            setBoardWidth(500);
-        } else {
-            setBoardWidth(viewPortWidth/2.5);
-        }
-        moveHistory = []
+        setMoveHistory([])
     }, [])
 
-    const makeMove = (move) => {
-        const gameBackup = game;
-        gameBackup.loadPgn(game.pgn())
-        const gameCopy = game;
-        gameCopy.loadPgn(game.pgn());
-        gameCopy.move(move);
-        setGame(gameCopy);
-        setPosition(game.fen())
-
-        moveHistory = gameCopy.history();
-        //console.log("last move played: " + moveHistory[moveHistory.length - 1])
-        nextMove = MoveSelector(moveHistory, openeingName, openingLineIndex)
-        //console.log(nextMove)
-
-        if(nextMove === "invalid"){
-            moveMessage = " is not the correct move"
-            movePlayed = moveHistory[moveHistory.length - 1]
-            setTimeout(() =>  {
-                game.undo();
-                setGame(gameBackup)
-                setPosition(gameBackup.fen());
-            }, 100)
-            
-        } else if(nextMove == null){
-            moveMessage = " is the correct move!"
-            movePlayed = moveHistory[moveHistory.length - 1]
-            moveMessage = "Opening Complete!"
-            movePlayed = ""
-            //console.log("move sequence complete")
-        } else {
-            setTimeout(() => {
-                moveMessage = " is the correct move!"
-                movePlayed = moveHistory[moveHistory.length - 1]
-
-
-                if(moveHistory.length === moveSequence.length - 1){
-                    moveMessage = "Opening Complete!"
-                    movePlayed = ""
-                    //console.log("move sequence complete")
-                }
-
-
-                playMove(nextMove)
-            }, 250);
-        }
-        console.log(moveMessage)
-    }
-
-    const playMove = (nextMove) => {
-        const gameCopy = game;
-        gameCopy.loadPgn(game.pgn());
-        gameCopy.move(nextMove);
-        setGame(gameCopy);
-        setPosition(game.fen())
-        moveHistory.push(nextMove)
-    }
-
-    const onDrop = (startSquare, endSquare) => {
-        makeMove({
-            from: startSquare,
-            to: endSquare,
-            promotion: "q",
-        });
-    }
-
-    const isDraggable = (piece, sourceSquare) => {
-        if(piece.piece === 'wP' || piece.piece === 'wR' || piece.piece === 'wB' || piece.piece === 'wN' || piece.piece === 'wK' || piece.piece === 'wQ'){
-        return true
-        } else return false
-    }
-
     function changeLine(){
-        let gameCopy = new Chess();
-        openingLine = getAlternateLine(openeingName, openingLine);
+        setOpeningLine(getAlternateLine(openeingName, openingLine));
         openingLineIndex = getLineIndex(openeingName, openingLine);
         moveSequence = getMoveSequence(openeingName, openingLine[openingLineIndex]);
-        setGame(gameCopy);
-        setPosition(gameCopy.fen());
-        moveHistory = []
+        setMoveHistory([])
+    }
+
+    function updateMoveHistory(history) {
+        setMoveHistory(history)
+    }
+
+    function setMoveResultFunc(result){
+        setMoveResult(result);
     }
 
     return(
@@ -150,12 +40,12 @@ function TrialPage() {
             <div className='content'>
                 <div className="row">
                     <div className="hc1">
-                        <Chessboard boardWidth={boardWidth} position={position} onPieceDrop={onDrop} isDraggablePiece={isDraggable} animationDuration={750}/>
+                        <TrainBoardWhite openingName={openeingName} openingLine={openingLine} moveHistory={moveHistory} updateMoveHistory={updateMoveHistory} setMoveResult={setMoveResultFunc}/>
                     </div>
                     <div className="hc2">
                         <h1>{openeingName}</h1>
                         
-                        <MoveTable moveSequence={moveSequence} moves={moveHistory} openingLine={openingLine}/>
+                        <MoveTable moveSequence={moveSequence} moves={moveHistory} openingLine={openingLine} moveResult={moveResult}/>
 
                         {/* <img src={image}/>
                         <h3>{movePlayed}{moveMessage}</h3> */}
